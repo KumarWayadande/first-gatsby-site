f97416c002c38a2fa17aa092ab9abc8f42260ecb
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-22 00:26:08 +0530

    React to Gatsby - Router Smiplification

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 // Article Component that creates a single artilce page
-// Article Component that creates a sin
+// Article Component that creates a sing
 const Article = () => {
   return (
     <div className="article-row flex flex-col md:flex-row">
