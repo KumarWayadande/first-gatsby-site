b29add112bf7b9b94f0de92b1c9f2d8d99c8bd11
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-22 00:35:03 +0530

    React to Gatsby - Removal of Context value for component construct

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 // this component refers to the main index route component
-// this component refers to the main index route compo
+// this component refers to the main index route compon
 
 
 import * as React from "react"
