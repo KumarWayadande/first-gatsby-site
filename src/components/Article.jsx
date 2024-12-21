b660dc0492f3bfe3ee1230d339fe160e238b7e97
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-22 00:21:26 +0530

    React to Gatsby - 1

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 // Article Component that creates a single artilce page
-// A
+// Ar
 const Article = () => {
   return (
     <div className="article-row flex flex-col md:flex-row">
