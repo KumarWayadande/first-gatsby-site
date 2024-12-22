fa0b08108ab126bdc9d85cdfc1face9ae39371f2
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-23 00:37:52 +0530

    graphql query for fetching frontmatter for ArticleContainer

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 // Article Component that creates a single article page
 // Article Component that creates a single article page
-// Article Componen
+// Article Component
 const Article = ({date, title, description}) => {
   return (
     <div className="article-row flex flex-col md:flex-row">
