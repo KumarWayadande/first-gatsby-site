d4cbd5eb256bae071a639bdea38f1207a74eac6b
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-23 00:33:24 +0530

    single article created using graphql query

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 // Article Component that creates a single article page
-// Article Component that creat
+// Article Component that create
 const Article = ({date, title, description}) => {
   return (
     <div className="article-row flex flex-col md:flex-row">
