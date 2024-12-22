3688dfb558725af9bd2556de438ecdf72c138499
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-23 00:34:29 +0530

    articles for home page getched from graphql data layer

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 // Article Component that creates a single article page
-// Article Component that creates a single
+// Article Component that creates a single a
 const Article = ({date, title, description}) => {
   return (
     <div className="article-row flex flex-col md:flex-row">
