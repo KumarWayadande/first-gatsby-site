f1e204cde28357dcc5e18c97cd776bf644b3df18
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-23 00:38:24 +0530

    DarkMode Handled using dark class using classlist

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 // Article Component that creates a single article page
 // Article Component that creates a single article page
-// Article Component th
+// Article Component tha
 const Article = ({date, title, description}) => {
   return (
     <div className="article-row flex flex-col md:flex-row">
