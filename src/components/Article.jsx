a88ecca462d7ef3dc152d5d992499e9efc3d689b
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-23 00:31:25 +0530

    dynamic companu images added

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 // Article Component that creates a single article page
-// Artic
+// Articl
 const Article = ({date, title, description}) => {
   return (
     <div className="article-row flex flex-col md:flex-row">
